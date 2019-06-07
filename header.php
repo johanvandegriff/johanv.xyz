a42ee57401519acbcc0e3a9bf0694e3a00623f62
Johan Vandegriff <johan@vandymail.com>
2019-06-07 19:12:13 -0400

    importing php from old site

---
+++ header.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html><html><head><meta charset="UTF-8">
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<![endif]-->
+<link rel="stylesheet" type="text/css" href="/style.css">
+<title>Johan.Vandegriff.net</title>
+<meta name="description" content="Johan Vandegriff's website where he shares his projects, blog, and games.">
+<link rel="icon" href="/favicon.ico?" type="image/x-icon">
+</head>
+<body>
+<div class="ribbon"><header><a href="/" class="green"><pre>
+    _____  _                 __        __           _              _  ___ ___                    
+   (_   _)| |                \ \      / /          | |            (_)/  _/  _|                _  
+     | |__| |__ __ ______     \ \    / __ ______ __| |____ ___   _ _ | / | /      _____ ____ | | 
+     | /  | __ / _` | __ )  __ \ \  / /  ` | __ /  \ /  _ / _ |\/_| |_ _|_ _| __  | __ / __ |_ _|
+  /\_| |()| || |(_| | || | |\_\ \ \/ /| -  | || | -  | ___\__ | / | || | | | |\_\ | ||_| \__/| |_
+  \____\__|_||_\__,_|_||_| \|_|  \__/ \__,_|_||_\__/_\___/__/ |_| |_||_| |_| \|_| |_||_\____||__/
+                                                         |___/                                   </pre>
+</a></header>
+<nav><ul>
+<?php
+$filename = 'https://www.johan.vandegriff.net/nav.txt';
+$contents = file($filename);
+
+if (strcmp($pageName, "") == 0) $pageName = $argv[1];
+
+$width = 100.0/count($contents);
+foreach($contents as $line) {
+  $parts = preg_split('/\t/', rtrim($line));
+  $name = $parts[0];
+  $url = $parts[1];
+  if (strcmp($pageName, $name) == 0){
+    $active = ' class="active"';
+  } else {
+    $active = '';
+  }
+  echo '<li'.$active.' style="width:'.$width.'%"><a href="'.$url.'">'.$name."</a></li>\n";
+}
+
+function hideEmail($email, $text) {
+  hideEmailOpts($email, $text, '');
+}
+
+function hideEmailOpts($email, $text, $options) {
+  $email = strrev($email);
+  $email = implode("'+'&#x40;'+'", explode("@", $email));
+  $email = implode("'+'&#x2e;'+'", explode(".", $email));
+  $email = "'" . $email . "'";
+  echo '<a ' . $options . 'href="#" onclick="this.innerHTML=' . "'&#x202e;'+" . $email . "+'&#x202d;'" . '">' . $text . '</a>';
+}
+
+?>
+</ul></nav></div>
+<div class="main">
+<!--<aside class="left"></aside>-->
+<aside class="right">
+<!--<p class="title">External Links</p>-->
+<p class="monospace green">
+$ cat <a target="_blank" class="green" href="/links.txt">links.txt</a><br/>
+<?php
+$filename = 'https://www.johan.vandegriff.net/links.txt';
+$contents = file($filename);
+
+foreach($contents as $line) {
+   $parts = preg_split('/\t/', rtrim($line));
+   echo '<br/><a target="_blank" class="green" href="' . $parts[1] . '">' . $parts[0] . "</a>\n";
+}
+
+?>
+<br/><br/><a class="green" href="#" onclick="this.innerHTML='&#x202e;'+'moc'+'&#x2e;'+'liamydnav'+'&#x40;'+'nahoj'+'&#x202d;'">tutanota</a><br/><a class="green" href="#" onclick="this.innerHTML='&#x202e;'+'moc'+'&#x2e;'+'liamnotorp'+'&#x40;'+'navjj'+'&#x202d;'">protonmail</a>
+
+</p></aside><section>
+<br/>
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php $pageName = "Home"; include 'header.php'; ?>
+
+<p>Welcome to my website! Here are a few of my projects:</p>
+
+<h3>ArduinOLED</h3>
+<p>I am currently working on a board with an OLED screen, buttons, a joystick, and alligator clip connectors. More info <a href="ArduinOLED">here</a>.</p>
+
+<h3>HexaGame</h3>
+<p>Another ongoing project is a handheld game called HexaGame that consists of a hexagon of LEDs with buttons. More info <a href="hexagame">here</a>.</p>
+
+<p><b>Online Games:</b></p>
+<ul>
+<li> <a href="boggle.py">Boggle Game</a> (written in python)</li>
+<li> <a href="maze/the_maze.php">The Maze Game</a> (perl)</li>
+<li> <a href="whack.pl">Whack-A-Mole</a> (perl)</li>
+<li> <a href="math/math_game.php">Math Quiz</a> (perl)</li>
+<li> <a href="http://scratch.mit.edu/users/jjvan/" target="frame">Scratch Games</a></li>
+</ul>
+
+<p><b>Python:</b></p>
+<ul>
+<li><a href="quest.py">Quest Game</a></li>
+<li><a href="calculator-page.php">Command-line calculator</a></li>
+</ul>
+<a target="_blank" rel="noreferrer noopener" href="https://nextcloud.com/federation#johanv@drive.confuzer.cloud" style="padding:10px;background-color:#383838;color:#ffffff;border-radius:3px;padding-left:4px;"> <span style="background-image:url(https://drive.confuzer.cloud/core/img/logo.svg);width:50px;height:30px;position:relative;top:8px;background-size:contain;display:inline-block;background-repeat:no-repeat; background-position: center center;"></span> Share with me via Nextcloud</a>
+<?php include 'footer.php'; ?>
