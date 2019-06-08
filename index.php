9c598c800d211159e595396ec5e27bd679b43941
Johan Vandegriff <johan@vandymail.com>
2019-06-08 13:17:41 -0400

    updated home page

--- index.php
+++ index.php
@@ -1,26 +1,18 @@
 <?php $pageName = "Home"; include 'header.php'; ?>
 
-<p>Welcome to my website! Here are a few of my projects:</p>
+<p>Hi, I'm Johan. This is my new website, which looks a lot like <a target=_blank href="https://johan.vandegriff.net">the old site</a>... Anyway, I have a <a target=_blank href="https://blog.johanv.xyz">blog</a> that has some joking posts and some informative posts, and some that are a mixture of both. Right now there isn't much here, but more is on the way <a target=_blank href="https://blog.johanv.xyz/how-i-created-johanv-xyz">thanks to Dokku</a>. Feel free to contact me, I have multiple ways to <a target=_blank href="contact.php">get in touch securely</a>.</p>
+
+<p>Here are a few of my old projects:</p>
 
 <h3>ArduinOLED</h3>
-<p>I am currently working on a board with an OLED screen, buttons, a joystick, and alligator clip connectors. More info <a href="ArduinOLED">here</a>.</p>
+<p>A board with an OLED screen, buttons, a joystick, and alligator clip connectors. More info <a href="ArduinOLED">here</a>.</p>
 
 <h3>HexaGame</h3>
-<p>Another ongoing project is a handheld game called HexaGame that consists of a hexagon of LEDs with buttons. More info <a href="hexagame">here</a>.</p>
+<p>A handheld game that consists of a hexagon of LEDs with buttons. More info <a href="hexagame">here</a>.</p>
 
-<p><b>Online Games:</b></p>
+<h3>Python Programs</h3>
 <ul>
-<li> <a href="boggle.py">Boggle Game</a> (written in python)</li>
-<li> <a href="maze/the_maze.php">The Maze Game</a> (perl)</li>
-<li> <a href="whack.pl">Whack-A-Mole</a> (perl)</li>
-<li> <a href="math/math_game.php">Math Quiz</a> (perl)</li>
-<li> <a href="http://scratch.mit.edu/users/jjvan/" target="frame">Scratch Games</a></li>
+<li><a href="calculator.php">Command-line calculator</a></li>
+<li><a href="quest.php">Quest Game</a></li>
 </ul>
-
-<p><b>Python:</b></p>
-<ul>
-<li><a href="quest.py">Quest Game</a></li>
-<li><a href="calculator-page.php">Command-line calculator</a></li>
-</ul>
-<a target="_blank" rel="noreferrer noopener" href="https://nextcloud.com/federation#johanv@drive.confuzer.cloud" style="padding:10px;background-color:#383838;color:#ffffff;border-radius:3px;padding-left:4px;"> <span style="background-image:url(https://drive.confuzer.cloud/core/img/logo.svg);width:50px;height:30px;position:relative;top:8px;background-size:contain;display:inline-block;background-repeat:no-repeat; background-position: center center;"></span> Share with me via Nextcloud</a>
 <?php include 'footer.php'; ?>
