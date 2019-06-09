bfb64cac8bd2205bdbc516720e59d07ae7ed8e31
Johan Vandegriff <johan@vandymail.com>
2019-06-09 00:48:26 -0400

    added shortcut to bag with ?h=

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<?php $pageName = "Home"; include 'header.php'; ?>
+<?php $pageName = "Home"; include 'header.php';
+if (isset($_GET['h']) && !empty($_GET['h'])) {
+  $link_name = $_GET['h'];
+  echo '<script>window.location="https://bag.johanv.xyz/f.php?h="' . $link_name . ';</script>'
+}
+?>
 
 <p>Hi, I'm Johan. This is my new website, which looks a lot like <a target=_blank href="https://johan.vandegriff.net">the old site</a>... Anyway, I have a <a href="https://blog.johanv.xyz">blog</a> that has some joking posts and some informative posts, and some that are a mixture of both. Right now there isn't much here, but more is on the way <a href="https://blog.johanv.xyz/how-i-created-johanv-xyz">thanks to Dokku</a>. Feel free to contact me, I have multiple ways to <a target=_blank href="contact">get in touch securely</a>.</p>
 
