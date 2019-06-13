d5cbb15960a9564b346aede045ed4bd254f1581e
Johan Vandegriff <johan@vandymail.com>
2019-06-13 03:02:32 -0400

    added last name :)

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if (isset($_GET['h']) && !empty($_GET['h'])) {
 }
 ?>
 
-<p>Hi, I'm Johan. This is my new website, which looks a lot like <a target=_blank href="https://johan.vandegriff.net">the old site</a>... Anyway, I have a <a href="https://blog.johanv.xyz">blog</a> that has some joking posts and some informative posts, and some that are a mixture of both. Right now there isn't much here, but more is on the way <a href="https://blog.johanv.xyz/how-i-created-johanv-xyz">thanks to Dokku</a>. Feel free to contact me, I have multiple ways to <a target=_blank href="contact">get in touch securely</a>.</p>
+<p>Hi, I'm Johan Vandegriff. This is my new website, which looks a lot like <a target=_blank href="https://johan.vandegriff.net">the old site</a>... Anyway, I have a <a href="https://blog.johanv.xyz">blog</a> that has some joking posts and some informative posts, and some that are a mixture of both. Right now there isn't much here, but more is on the way <a href="https://blog.johanv.xyz/how-i-created-johanv-xyz">thanks to Dokku</a>. Feel free to contact me, I have multiple ways to <a target=_blank href="contact">get in touch securely</a>.</p>
 
 <p>Here are a few of my old projects:</p>
 
