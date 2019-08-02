acec5e86390b1211039b573233009cc5abf98bd7
Johan Vandegriff <johan@vandymail.com>
2019-08-02 00:18:22 -0400

    fixed typo, added newlines

--- projects/index.php
+++ projects/index.php
@@ -4,6 +4,8 @@
 
 <p>Most of my software projects are hosted on <a target="_blank" href="https://gitlab.com/johanvandegriff">GitLab</a>. Here are a few highlights:</p>
 
+<br/>
+
 <h3><a target="_blank" href="">BoggleSolver</a></h3>
 <p>Python program that solves a word puzzle called Boggle.</p>
 
@@ -16,13 +18,13 @@
 <h3><a target="_blank" href="">zfs-server-cluster-setup</a></h3>
 <p>Setup scripts to create an Ubuntu server kubernetes cluster booting from ZFS with remote desktop</p>
 
-
+<br/>
 <p>Here are a few of my other projects:</p>
 
 <h3><a href="ArduinOLED">ArduinOLED</a></h3>
 <p>A board with an OLED screen, buttons, a joystick, and alligator clip connectors.</p>
 
-<h3><a href="hexagame">HexaGame/a></h3>
+<h3><a href="hexagame">HexaGame</a></h3>
 <p>A handheld game that consists of a hexagon of LEDs with buttons.</p>
 
 <h3>Python Programs</h3>
