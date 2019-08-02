b9e71c79c89afa3853ab35e35bcddf47f255b557
Johan Vandegriff <johan@vandymail.com>
2019-08-01 23:53:00 -0400

    added more videos

--- videos/index.php
+++ videos/index.php
@@ -1,6 +1,25 @@
 <?php $pageName = "Videos"; include '../header.php'; ?>
 
 <h1 style="text-align: center; ">Videos</h1>
+<!--Maker's Case Gemstone Commercial-->
 <iframe width="560" height="315" sandbox="allow-same-origin allow-scripts" src="https://peertube.social/videos/embed/bf19ee3e-b4e2-484d-ae08-bd52b2bf6c1e" frameborder="0" allowfullscreen></iframe>
+
+<!--Project eV-->
+<iframe width="560" height="315" sandbox="allow-same-origin allow-scripts" src="https://peertube.social/videos/embed/2fe7dcfd-3428-462d-ba7e-7aa5e18e75ea" frameborder="0" allowfullscreen></iframe>
+
+<!--Minecraft Minecart Mail System-->
+<iframe width="560" height="315" sandbox="allow-same-origin allow-scripts" src="https://peertube.social/videos/embed/989b12b4-7324-4c22-9cef-33f9fd7de385" frameborder="0" allowfullscreen></iframe>
+
+<!--Obnoxious Scheme to Convince All to Recycle (OSCAR)-->
+<iframe width="560" height="315" sandbox="allow-same-origin allow-scripts" src="https://peertube.social/videos/embed/66d762b2-484b-4ac7-b1b7-665809c2d162" frameborder="0" allowfullscreen></iframe>
+
+<!--NARCISSIST LIP SYNC *EMOTIONAL*-->
+<iframe width="560" height="315" sandbox="allow-same-origin allow-scripts" src="https://peertube.social/videos/embed/89cf5dc0-9f41-410c-ad66-5c9c99fd813a" frameborder="0" allowfullscreen></iframe>
+
+<!--The Untold TRUTH Behind The SOLAR 'ECLIPSE' 2017! | TRUTH DETECTORS-->
+<iframe width="560" height="315" sandbox="allow-same-origin allow-scripts" src="https://peertube.social/videos/embed/77abd68c-4d36-4dcc-82d8-74b36a499289" frameborder="0" allowfullscreen></iframe>
+
+<!--The Untold TRUTH Behind The Parker Solar Probe 2018! | TRUTH DETECTORS-->
+<iframe width="560" height="315" sandbox="allow-same-origin allow-scripts" src="https://peertube.social/videos/embed/4a8164e1-1ea3-4d26-9505-0d2d7d8d3731" frameborder="0" allowfullscreen></iframe>
 
 <?php include '../footer.php'; ?>
