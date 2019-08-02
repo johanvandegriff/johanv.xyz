5290b180f5db03063feb29605dc4654b309d888f
Johan Vandegriff <johan@vandymail.com>
2019-08-02 00:15:51 -0400

    added GitLab projects

--- index.php
+++ index.php
@@ -5,19 +5,7 @@ if (isset($_GET['h']) && !empty($_GET['h'])) {
 }
 ?>
 
-<p>Hi, I'm Johan Vandegriff. This is my new website, which looks a lot like <a target=_blank href="https://johan.vandegriff.net">the old site</a>... Anyway, I have a <a href="https://blog.johanv.xyz">blog</a> that has some joking posts and some informative posts, and some that are a mixture of both. Right now there isn't much here, but more is on the way <a href="https://blog.johanv.xyz/how-i-created-johanv-xyz">thanks to Dokku</a>. Feel free to contact me, I have multiple ways to <a target=_blank href="contact">get in touch securely</a>.</p>
+<p>Hi, I'm Johan Vandegriff. This is my new website, which looks a lot like <a target=_blank href="https://johan.vandegriff.net">the old site</a>... Anyway, I have a <a href="https://blog.johanv.xyz">blog</a> that has some joking posts and some informative posts, and some that are a mixture of both. Right now there isn't much here, but more is on the way <a href="https://blog.johanv.xyz/how-i-created-johanv-xyz">thanks to Dokku</a>.
 
-<p>Here are a few of my old projects:</p>
-
-<h3>ArduinOLED</h3>
-<p>A board with an OLED screen, buttons, a joystick, and alligator clip connectors. More info <a href="ArduinOLED">here</a>.</p>
-
-<h3>HexaGame</h3>
-<p>A handheld game that consists of a hexagon of LEDs with buttons. More info <a href="hexagame">here</a>.</p>
-
-<h3>Python Programs</h3>
-<ul>
-<li><a href="calculator">Command-line calculator</a></li>
-<li><a href="quest">Quest Game</a></li>
-</ul>
+<p>A great place to start is watching some of my <a href="/videos">videos</a> or checking out some of my <a href="/projects">projects</a>! Feel free to contact me, I have multiple ways to <a target=_blank href="contact">get in touch securely</a>.</p>
 <?php include 'footer.php'; ?>
---
+++ projects/index.php
@@ -0,0 +1,33 @@
+<?php $pageName = "Projects"; include '../header.php'; ?>
+
+<h1 style="text-align: center; ">Johan's Projects</h1>
+
+<p>Most of my software projects are hosted on <a target="_blank" href="https://gitlab.com/johanvandegriff">GitLab</a>. Here are a few highlights:</p>
+
+<h3><a target="_blank" href="">BoggleSolver</a></h3>
+<p>Python program that solves a word puzzle called Boggle.</p>
+
+<h3><a target="_blank" href="">Stoichiometry</a></h3>
+<p>Java project that balances reactions and computes grams/moles. Includes fractional equation solver.</p>
+
+<h3><a target="_blank" href="">Warlike</a></h3>
+<p>Card game based on War</p>
+
+<h3><a target="_blank" href="">zfs-server-cluster-setup</a></h3>
+<p>Setup scripts to create an Ubuntu server kubernetes cluster booting from ZFS with remote desktop</p>
+
+
+<p>Here are a few of my other projects:</p>
+
+<h3><a href="ArduinOLED">ArduinOLED</a></h3>
+<p>A board with an OLED screen, buttons, a joystick, and alligator clip connectors.</p>
+
+<h3><a href="hexagame">HexaGame/a></h3>
+<p>A handheld game that consists of a hexagon of LEDs with buttons.</p>
+
+<h3>Python Programs</h3>
+<ul>
+<li><a href="calculator">Command-line calculator</a></li>
+<li><a href="quest">Quest Game</a></li>
+</ul>
+<?php include '../footer.php'; ?>
