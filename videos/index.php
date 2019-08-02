8ea30cf0988ac7d797df6ac6094406ffe268a8da
Johan Vandegriff <johan@vandymail.com>
2019-08-01 23:42:25 -0400

    added video page

---
+++ videos/index.php
@@ -0,0 +1,6 @@
+<?php $pageName = "Videos"; include '../header.php'; ?>
+
+<h1 style="text-align: center; ">Videos</h1>
+<iframe width="560" height="315" sandbox="allow-same-origin allow-scripts" src="https://peertube.social/videos/embed/bf19ee3e-b4e2-484d-ae08-bd52b2bf6c1e" frameborder="0" allowfullscreen></iframe>
+
+<?php include '../footer.php'; ?>
