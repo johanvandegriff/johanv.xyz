c44bebfd71e628be5a29c22e792ca8d99bb5753c
Johan Vandegriff <johan@vandymail.com>
2019-08-06 14:32:37 -0400

    added mastodon verification

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if (isset($_GET['h']) && !empty($_GET['h'])) {
 
 <p>Hi, I'm Johan Vandegriff. This is my new website, which looks a lot like <a target=_blank href="https://johan.vandegriff.net">the old site</a>... Anyway, I have a <a href="https://blog.johanv.xyz">blog</a> that has some joking posts and some informative posts, and some that are a mixture of both. Right now there isn't much here, but more is on the way <a href="https://blog.johanv.xyz/how-i-created-johanv-xyz">thanks to Dokku</a>.
 
-<p>A great place to start is watching some of my <a href="/videos">videos</a> or checking out some of my <a href="/projects">projects</a>! Feel free to contact me, I have multiple ways to <a target=_blank href="contact">get in touch securely</a>.</p>
+<p>A great place to start is watching some of my <a href="/videos">videos</a> or checking out some of my <a href="/projects">projects</a>! Feel free to contact me, I have multiple ways to <a target=_blank href="contact">get in touch securely</a>. You can also follow me on <a rel="me" href="https://fosstodon.org/@johanv">Mastodon</a>.</p>
 
 <h3>Other People's Sites</h3>
 <ul>
