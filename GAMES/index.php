ed86a34b2b34984aaa7cc6e683e204bcc54d84da
Johan Vandegriff <johan@vandymail.com>
2019-09-03 16:34:13 -0400

    added picture to GAMES

--- GAMES/index.php
+++ GAMES/index.php
@@ -2,6 +2,8 @@
 
 <h1 style="text-align: center; ">The Glamorous Automatic Multi-game Emulator System (GAMES)</h1>
 
-<p>GAMES is a tiny Arduino game system with a 3x3 LED screen and 4 buttons. It is a work in progress.</p>
+<p>GAMES is a tiny Arduino game system with a 3x3 LED screen and 4 buttons. Version 1, which uses an Arduino Pro Mini, is done; and version 2, which will use an ATTiny microcontroller, is a work in progress.</p>
+
+<img style="width:65%; display:block; margin-left:auto; margin-right:auto" src="/gallery/galleries/Random/games-1.JPG">
 
 <?php include '../footer.php'; ?>
