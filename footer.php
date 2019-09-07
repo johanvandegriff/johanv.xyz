942c8470e851a15b3324da8e635571b388ce9ea6
Johan Vandegriff <johan@vandymail.com>
2019-09-06 22:37:37 -0400

    added license to html

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
 
 </section>
+<footer>
+<h6>This site's <a target="_blank" href="https://gitlab.com/johanvandegriff/johanv.xyz">source code</a> is released under the GNU AGPLv3.</h6>
+</footer>
 </div>
 </body>
 </html>
