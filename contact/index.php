bcb9aac863de6e9ff57fd9bbd6c458ac463c3ef3
Johan Vandegriff <johan@vandymail.com>
2019-11-05 13:20:26 -0500

    added basic email to contact page

--- contact/index.php
+++ contact/index.php
@@ -1,5 +1,8 @@
 <?php $pageName = "Contact"; $description = "Johan Vandegriff's contact info and ways to contact him securely with Tutanota, Protonmail, or Keybase."; include $_SERVER['DOCUMENT_ROOT'].'/header.php'; ?>
 
+<h2>How to Contact Me Simply</h2>
+<p>Email me at <a  id="9dda4e98_2" href="#9dda4e98_2" onclick="this.innerHTML='&#x202e;'+'moc'+'&#x2e;'+'liamydnav'+'&#x40;'+'nahoj'+'&#x202d;'">[click to show]</a></p>
+
 <h2>How to Contact Me Securely</h2>
 <p>These are some easy ways to securely send me a message with end-to-end encryption.</p>
 
@@ -8,7 +11,7 @@
 <a target=_blank href="https://mail.tutanota.com/signup">
 at tutanota.com</a>
 to send me an encrypted message at:</p>
-<a  id="9dda4e98_2" href="#9dda4e98_2" onclick="this.innerHTML='&#x202e;'+'moc'+'&#x2e;'+'liamydnav'+'&#x40;'+'nahoj'+'&#x202d;'">[click to show]</a>
+<a  id="9dda4e98_3" href="#9dda4e98_3" onclick="this.innerHTML='&#x202e;'+'moc'+'&#x2e;'+'liamydnav'+'&#x40;'+'nahoj'+'&#x202d;'">[click to show]</a>
 <?php $GLOBALS['email_counter']++; ?>
 
 <h3>Protonmail</h3>
@@ -16,7 +19,7 @@ to send me an encrypted message at:</p>
 <a target=_blank href="https://protonmail.com/signup">
 protonmail.com</a>
 to send me an encrypted message at:</p>
-<a  id="9dda4e98_3" href="#9dda4e98_3" onclick="this.innerHTML='&#x202e;'+'moc'+'&#x2e;'+'liamnotorp'+'&#x40;'+'navjj'+'&#x202d;'">[click to show]</a>
+<a  id="9dda4e98_4" href="#9dda4e98_4" onclick="this.innerHTML='&#x202e;'+'moc'+'&#x2e;'+'liamnotorp'+'&#x40;'+'navjj'+'&#x202d;'">[click to show]</a>
 <?php $GLOBALS['email_counter']++; ?>
 
 <h3>Keybase</h3>
