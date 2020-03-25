832f4ecd3cb8029ff6fba9a95c357c613f7bd77e
Johan Vandegriff <johan@vandymail.com>
2020-03-25 07:55:57 -0400

    added LBRY invite link

--- videos/index.php
+++ videos/index.php
@@ -2,18 +2,32 @@
 
 <p>Here are some of my videos! You can see all of them on <a target="_blank" href="https://peertube.social/video-channels/jjvantheman_channel/videos">PeerTube</a> or <a target="_blank" href="https://www.youtube.com/user/jjvantheman">YouTube</a>. PeerTube is an alternative decentralized video platform to YouTube and I use it because I don't have to be locked in to a large company (Google in this case). With PeerTube, I have a variety of choices of where to host my videos, and if I wanted to, I could even host them myself in my basement or in the cloud! Right now, I'm using <a href="https://peertube.social/">peertube.social</a>, which is one of the most popular general purpose instances.</p>
 
+<!-- <iframe src="https://spee.ch/video-embed/@johanv:5/makers-case-gemstone-commercial" allowfullscreen="true" style="border:0"></iframe> -->
+<!-- <iframe width="560" height="315" src="https://lbry.tv/$/embed/makers-case-gemstone-commercial/e3e0edee20fbab97268a8e7dfcc35c798cd74d6c" allowfullscreen></iframe> -->
 <iframe title="Maker's Case Gemstone Commercial" width="560" height="315" sandbox="allow-same-origin allow-scripts" src="https://peertube.social/videos/embed/bf19ee3e-b4e2-484d-ae08-bd52b2bf6c1e" frameborder="0" allowfullscreen></iframe>
 
+<!-- <iframe src="https://spee.ch/video-embed/@johanv:5/project-eV" allowfullscreen="true" style="border:0"></iframe> -->
+<!-- <iframe width="560" height="315" src="https://lbry.tv/$/embed/project-eV/15d9aae75b3f9b7fd93d543290da9390721e58a8" allowfullscreen></iframe> -->
 <iframe title="Project eV" width="560" height="315" sandbox="allow-same-origin allow-scripts" src="https://peertube.social/videos/embed/2fe7dcfd-3428-462d-ba7e-7aa5e18e75ea" frameborder="0" allowfullscreen></iframe>
 
+<!-- <iframe src="https://spee.ch/video-embed/@johanv:5/minecart-mail" allowfullscreen="true" style="border:0"></iframe> -->
+<!-- <iframe width="560" height="315" src="https://lbry.tv/$/embed/minecart-mail/4e7a5bcab4ec162a31fc18f41283a308e4cdc27d" allowfullscreen></iframe> -->
 <iframe title="Minecraft Minecart Mail System" width="560" height="315" sandbox="allow-same-origin allow-scripts" src="https://peertube.social/videos/embed/989b12b4-7324-4c22-9cef-33f9fd7de385" frameborder="0" allowfullscreen></iframe>
 
+<!-- spee.ch -->
+<!-- <iframe width="560" height="315" src="https://lbry.tv/$/embed/OSCAR/467bb95919dc9546619521ce2807a8176a372832" allowfullscreen></iframe> -->
 <iframe title="Obnoxious Scheme to Convince All to Recycle (OSCAR)" width="560" height="315" sandbox="allow-same-origin allow-scripts" src="https://peertube.social/videos/embed/66d762b2-484b-4ac7-b1b7-665809c2d162" frameborder="0" allowfullscreen></iframe>
 
+<!-- <iframe src="https://spee.ch/video-embed/@johanv:5/NARCISSIST-LIP-SYNC-EMOTIONAL" allowfullscreen="true" style="border:0"></iframe> -->
+<!-- <iframe width="560" height="315" src="https://lbry.tv/$/embed/NARCISSIST-LIP-SYNC-EMOTIONAL/13df5146bcf8046f9eb71e16b9b55726c32e8876" allowfullscreen></iframe> -->
 <iframe title="NARCISSIST LIP SYNC *EMOTIONAL*" width="560" height="315" sandbox="allow-same-origin allow-scripts" src="https://peertube.social/videos/embed/89cf5dc0-9f41-410c-ad66-5c9c99fd813a" frameborder="0" allowfullscreen></iframe>
 
+<!-- spee.ch -->
+<!-- <iframe width="560" height="315" src="https://lbry.tv/$/embed/truth-detectors-solar-eclipse-2017/e99bc87516d03949a32f994969eeb8904a99ba50" allowfullscreen></iframe> -->
 <iframe title="The Untold TRUTH Behind The SOLAR 'ECLIPSE' 2017! | TRUTH DETECTORS" width="560" height="315" sandbox="allow-same-origin allow-scripts" src="https://peertube.social/videos/embed/77abd68c-4d36-4dcc-82d8-74b36a499289" frameborder="0" allowfullscreen></iframe>
 
+<!-- <iframe src="https://spee.ch/video-embed/@johanv:5/truth-detectors-parker-solar-probe-2018" allowfullscreen="true" style="border:0"></iframe> -->
+<!-- <iframe width="560" height="315" src="https://lbry.tv/$/embed/truth-detectors-parker-solar-probe-2018/4ff40337ff0642c79922ef4c7053c21090c0bd24" allowfullscreen></iframe> -->
 <iframe title="The Untold TRUTH Behind The Parker Solar Probe 2018! | TRUTH DETECTORS" width="560" height="315" sandbox="allow-same-origin allow-scripts" src="https://peertube.social/videos/embed/4a8164e1-1ea3-4d26-9505-0d2d7d8d3731" frameborder="0" allowfullscreen></iframe>
 
 <?php include $_SERVER['DOCUMENT_ROOT'].'/footer.php'; ?>
