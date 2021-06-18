5d684aeba2cae9f3aa689ecc9b4c5cfdc747044e
Johan Vandegriff <johan@vandymail.com>
2021-06-18 00:00:12 -0400

    small resume edits, fixed arrows overlapping

--- resume/logos/odysee.sh
+++ resume/logos/odysee.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-convert odysee_original.png -gravity center -resize 256x256 -extent 512x512 -threshold 95% -negate -transparent black odysee.png
+convert odysee_original.png -gravity center -resize 300x300 -extent 512x512 -threshold 95% -negate -transparent black odysee.png
+#convert odysee_original.png -gravity center -resize 256x256 -extent 512x512 -threshold 95% -negate -transparent black odysee.png
