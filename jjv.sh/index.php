0430da96709fd0a8b9666598ce971f23dc2eeae9
Johan Vandegriff <johan@vandymail.com>
2021-08-11 16:49:40 -0400

    added jjv.sh info page

---
+++ jjv.sh/index.php
@@ -0,0 +1,15 @@
+<?php $pageName = "jjv.sh"; $description = "About jjv.sh, my personal URL shortener."; include $_SERVER['DOCUMENT_ROOT'].'/header.php'; ?>
+
+<h1 style="text-align: center;">jjv.sh</h1>
+
+<p>jjv.sh is my personal URL shortener. It stands for <u>J</u>ohan <u>J</u>. <u>V</u>andegriff's <u>sh</u>ort links. Here are some example short links:</p>
+
+<ul>
+  <li><a href="https://jjv.sh/about">jjv.sh/about</a></li>
+  <li><a href="https://jjv.sh/boggle">jjv.sh/boggle</a></li>
+  <li><a href="https://jjv.sh/charlie">jjv.sh/charlie</a></li>
+  <li><a href="https://jjv.sh/resume">jjv.sh/resume</a></li>
+  <li><a href="https://jjv.sh/carl">jjv.sh/carl</a></li>
+</ul>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/footer.php'; ?>
