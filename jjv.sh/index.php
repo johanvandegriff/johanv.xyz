0119acc5dc4a79870c89e140251f3cf76a67a7a5
Johan Vandegriff <johan@vandymail.com>
2021-08-13 11:27:55 -0400

    added more short urls

--- jjv.sh/index.php
+++ jjv.sh/index.php
@@ -5,11 +5,12 @@
 <p>jjv.sh is my personal URL shortener. It stands for <u>J</u>ohan <u>J</u>. <u>V</u>andegriff's <u>sh</u>ort links. Here are some example short links:</p>
 
 <ul>
-  <li><a href="https://jjv.sh/about">jjv.sh/about</a></li>
-  <li><a href="https://jjv.sh/boggle">jjv.sh/boggle</a></li>
-  <li><a href="https://jjv.sh/charlie">jjv.sh/charlie</a></li>
-  <li><a href="https://jjv.sh/resume">jjv.sh/resume</a></li>
-  <li><a href="https://jjv.sh/carl">jjv.sh/carl</a></li>
+  <li><a href="https://jjv.sh/music">jjv.sh/music</a> - all the music me and my brother have made</li>
+  <li><a href="https://jjv.sh/boggle">jjv.sh/boggle</a> - play my boggle game online with friends</li>
+  <li><a href="https://jjv.sh/about">jjv.sh/about</a> - a bit about me</li>
+  <li><a href="https://jjv.sh/charlie">jjv.sh/charlie</a> - pictures of the cute doggo</li>
+  <li><a href="https://jjv.sh/carl">jjv.sh/carl</a> - a chatbot</li>
+  <li><a href="https://jjv.sh/resume">jjv.sh/resume</a> - my online resume</li>
 </ul>
 
 <?php include $_SERVER['DOCUMENT_ROOT'].'/footer.php'; ?>
